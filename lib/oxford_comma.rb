5f555dba0f754cb8f5f9dfaff5e58bcfca9785ce
Abraham Moreno <adiaz2821@gmail.com>
2019-10-18 13:59:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(list)
 if list == ["kiwi"]
   puts list
-else if list == ["kiwi", "durian"]
+else  list == ["kiwi", "durian"]
   puts list.join(" and ")
-else  list == ["kiwi", "durian", "starfruit"]
-  puts list.join(" , " " and ")
 end 
 end 
 
