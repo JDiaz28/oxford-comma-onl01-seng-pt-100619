59fd6c9ff0123774c1305cba94762de31ce437a1
Abraham Moreno <adiaz2821@gmail.com>
2019-10-18 14:35:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(list)
 if list.length(1)
   puts list
-else if list.length(2)
+elsif list.length(2)
   puts list.join(" and ")
-else if list.length(3)
+elsif list.length(3)
   puts list.join(", " " and ")
 else 
   puts "this"
