b52481d442779ee4aaabf4fc9b4e1ed25aab60d2
Abraham Moreno <adiaz2821@gmail.com>
2019-10-18 15:33:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if list.length == 1
   return list.join
 elsif list.length == 2
   return list.join(" and ")
-elsif list.length == 3
+elsif list.length == >= 3
   list.last.insert(0, "and ")
   return list.join(", ")
 else 
